0742d3bd5ebc7719238a546609b8fd843e712da3
tuanthoinguyen76-ops <tuanthoinguyen76@gmail.com>
2025-10-28 13:37:07 +0700

    Update service-worker.js

--- Nhat-CamnangVisa_PWA/service-worker.js
+++ Nhat-CamnangVisa_PWA/service-worker.js
@@ -1,20 +1,67 @@
-const CACHE_NAME = 'nhat-camnangvisa-v1';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json'
+const CACHE_NAME = "nhat-camnangvisa-v2.1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./tailwind.css",  
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("[SW] Caching all app shell files");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting(); 
 });
 
-self.addEventListener('fetch', event => {
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("[SW] Removing old cache:", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// ⚡ Bắt yêu cầu mạng (fetch)
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+     if (response) return response;
+     return fetch(event.request)
+        .then(networkResponse => {
+          if (
+            !networkResponse ||
+            networkResponse.status !== 200 ||
+            networkResponse.type !== "basic"
+          ) {
+            return networkResponse;
+          }
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+          return networkResponse;
+        })
+        .catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("./index.html");
+          }
+        });
     })
   );
 });
